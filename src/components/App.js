a0b492e6a2ff667c9b0769c7076a1111e434dc9e
Eliiisa <elisaklein66@gmail.com>
2021-06-13 16:18:37 +0200

    Initialisation Header.js + ajout dans App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import AddContact from './AddContact';
-import ContactList from './ContactList';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
     </div>
   );
 }
