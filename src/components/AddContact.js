7451d0b490ca3ffba3c3b5bc50a3b7ead08e7ba7
Eliiisa <elisaklein66@gmail.com>
2021-06-13 18:20:23 +0200

    Création d'un contact

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
 class AddContact extends React.Component {
+    state = {
+        name: "",
+        email: ""
+    };
+    add = (e) => {
+        e.preventDefault();
+        if (this.state.name === "" || this.state.email === "") {
+            alert("All the fields are mandatory !");
+            return;
+        };
+        this.props.addContactHandler(this.state);
+        this.setState({ name: "", email: "" });
+    };
+
     render() {
         return (
             <div className="ui main">
                 <h2>Ajouter un contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Nom</label>
-                        <input type="text" name="name" placeholder="Nom" />
+                        <input type="text" name="name" placeholder="Nom" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
                     </div>
                     <div className="field">
                         <label>Mail</label>
-                        <input type="email" name="mail" placeholder="Mail" />
+                        <input type="email" name="mail" placeholder="Mail" value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })} />
                     </div>
                     <button className="ui button blue">Ajouter</button>
                 </form>
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact])
+  };
+  // enregister en local la liste des contacts ajoutés
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts]);
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
     </div>
   );
