958e38a1ca98b0e2659af7abcc7c97aa30aaa355
Eliiisa <elisaklein66@gmail.com>
2021-06-13 17:58:02 +0200

    Affichage liste de contact (création du component ContactCard)

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-
-  const contacts = [
-    {
-      id: "1",
-      name: "John",
-      email: "john.doe@gmail.com"
-    },
-    {
-      id: "2",
-      name: "Johanna",
-      email: "johnna.doe@gmail.com"
-    }
-  ]
+  const [contacts, setContacts] = useState([]);
   return (
     <div className="ui container">
       <Header />
