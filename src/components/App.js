f01d3b7d7d91526e388cdcbf8d6a31053d619f3c
Eliiisa <elisaklein66@gmail.com>
2021-06-13 16:06:49 +0200

    mise en place du projet et création des components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
