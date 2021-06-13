97e26aae85583beed2870925098bc1836117fc12
Eliiisa <elisaklein66@gmail.com>
2021-06-13 17:25:23 +0200

    Ajout des components dans App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
   return (
