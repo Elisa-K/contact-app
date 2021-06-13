cc9939c96accce1fd930bee5c7ba80d705f02975
Eliiisa <elisaklein66@gmail.com>
2021-06-13 17:17:07 +0200

    Création const component ContactList

---
+++ src/components/ContactList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactList = () => {
+    return (
+        <div className="ui celled list">
+            Liste de contact
+        </div>
+    )
+}
+
+export default ContactList;
